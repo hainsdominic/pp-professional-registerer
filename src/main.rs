453a9eae8d60594e4dfbc3ce24ece4e82712fb9b
Dominic Hains <contact.dominichains@gmail.com>
2022-04-21 16:13:05 -0400

    feat: admin login

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use rpassword::read_password;
+use serde::Deserialize;
+use std::collections::HashMap;
+use text_io::read;
+
+#[derive(Deserialize)]
+struct AdminToken {
+    token: String,
+}
+
+fn main() {
+    let mut map = HashMap::new();
+
+    print!("Admin email: ");
+    std::io::Write::flush(&mut std::io::stdout()).expect("flush failed!");
+    let admin_email: String = read!();
+    map.insert("email", &admin_email);
+
+    print!("Admin password: ");
+    std::io::Write::flush(&mut std::io::stdout()).expect("flush failed!");
+    let admin_password: String = read_password().unwrap();
+    map.insert("password", &admin_password);
+
+    let client = reqwest::blocking::Client::new();
+    let res = client
+        .post("https://api.patientprogress.ca/api/auth")
+        .json(&map)
+        .send();
+
+    let response_text = res.unwrap().text().unwrap();
+
+    let admin_token: AdminToken = match serde_json::from_str(&response_text) {
+        Ok(admin_token) => admin_token,
+        Err(_) => {
+            println!("Invalid credentials");
+            return;
+        }
+    };
+
+    println!("{:?}", admin_token.token);
+}
